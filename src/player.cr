a657cee1e8e2c263b9c586da8831f9c2ae5b8b33
Will Lewis <will@wflewis.com>
2018-01-03 13:19:56 -0500

    Lots of changes

---
+++ src/player.cr
@@ -0,0 +1,31 @@
+module Walkabout
+  VERSION = YAML.parse(File.read("shard.yml"))["version"]
+
+  class Player
+    property :x, :y, :speed, :moving_right
+    @x : Int32
+    @y : Int32
+    @speed : Int32 = 100
+    @moving_right : Bool = true
+
+    def initialize(@x : Int32, @y : Int32)
+    end
+
+    def draw(m : Molly)
+      m.set_color(SDL::Color.new(240, 100, 100))
+      m.draw_rect(@x, @y, 100, 100)
+    end
+
+    def inspect(io)
+      io << "Player{(#{x}, #{y}), "
+      if @moving_right
+        io << "->"
+      else
+        io << "<-"
+      end
+      io << " @ #{@speed}}\n"
+    end
+  end
+end
+
+alias Player = Walkabout::Player
--- src/walkabout.cr
+++ src/walkabout.cr
@@ -1,14 +1,35 @@
-require "./walkabout/*"
+require "yaml"
 require "molly2d"
+require "sdl"
+require "./*"
 
 class Molly
   def load
-    puts "walkabout load"
+    puts "walkabout load v#{Walkabout::VERSION}"
+    @player = Player.new(50, 50)
   end
 
   def update(dt)
+    @player.try do |p|
+      if p.moving_right
+        p.x += (p.speed * dt).to_i
+      else
+        p.x -= (p.speed * dt).to_i
+      end
+
+      if p.x > 800 - 100
+        p.moving_right = false
+      end
+
+      if p.x < 0
+        p.moving_right = true
+      end
+    end
   end
 
   def draw
+    @player.try &.draw(self)
   end
 end
+
+Molly2d.run
