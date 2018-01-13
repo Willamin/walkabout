471ad0525296b9cd4ba09ba1b09258d3d5370204
Will Lewis <will@wflewis.com>
2018-01-13 17:20:53 -0500

    Add movement speed restrictions

--- src/objects/player.cr
+++ src/objects/player.cr
@@ -1,26 +1,42 @@
 module Walkabout
+  class MovementVector
+    property x, y
+
+    def initialize(@x : Float64 = 0_f64, @y : Float64 = 0_f64)
+    end
+  end
+
   class Player < Entity
     @speed = 200
     @facing_left = false
 
     def update(dt, m : Molly)
+      movement = MovementVector.new
       if m.keyboard_pressed?(Key::RIGHT)
-        @x += (@speed * dt).to_i
+        movement.x += (@speed * dt).to_i
         @facing_left = false
       end
 
       if m.keyboard_pressed?(Key::UP)
-        @y -= (@speed * dt).to_i
+        movement.y -= (@speed * dt).to_i
       end
 
       if m.keyboard_pressed?(Key::LEFT)
-        @x -= (@speed * dt).to_i
+        movement.x -= (@speed * dt).to_i
         @facing_left = true
       end
 
       if m.keyboard_pressed?(Key::DOWN)
-        @y += (@speed * dt).to_i
+        movement.y += (@speed * dt).to_i
       end
+
+      if movement.x != 0 && movement.y != 0
+        movement.x *= 0.707
+        movement.y *= 0.707
+      end
+
+      @x += movement.x.to_i
+      @y += movement.y.to_i
     end
 
     def draw(m : Molly)
